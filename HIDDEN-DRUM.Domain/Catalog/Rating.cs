8342bac605395aa44b032c3ecbcc3d5bfce533b0
Dylan Neff <neff.448@osu.edu>
2025-03-26 20:49:10 -0400

    more rating

--- HIDDEN-DRUM.Domain/Catalog/Rating.cs
+++ HIDDEN-DRUM.Domain/Catalog/Rating.cs
@@ -4,6 +4,18 @@ public class Rating
 {
     public int Stars { get; set; }
     public string UserName { get; set; }
-    public string review { get; set; }
+    public string Review { get; set; }
+}
 
+public Rating(int Stars, string UserName, string Review){
+    if ( stars < 1 ||  stars > 5){
+        throw new ArguementException("Star rating must be an integer of: 1 , 2, 3, 4, 5,. ");
+  }
+  if(string.isNullorEmpty(UserName))
+  {throw new ArguementException( UserName cannt be null. "");
+
+}
+this.Stars = stars;
+this.UserName = UserName; 
+this.Review = review;
 }
