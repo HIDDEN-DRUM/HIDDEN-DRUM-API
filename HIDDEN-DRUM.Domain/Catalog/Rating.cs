51df63d22b1bb9145d16e56d22101ade0e165d4f
Dylan Neff <neff.448@osu.edu>
2025-03-26 20:34:28 -0400

    rating

---
+++ HIDDEN-DRUM.Domain/Catalog/Rating.cs
@@ -0,0 +1,9 @@
+namespace HIDDEN-DRUM.Domain.Catalog;
+
+public class Rating 
+{
+    public int Stars { get; set; }
+    public string UserName { get; set; }
+    public string review { get; set; }
+
+}
