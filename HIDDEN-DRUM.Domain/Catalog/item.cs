19cea387477d9e80642562e819de3b633db6358b
Dylan Neff <neff.448@osu.edu>
2025-03-26 20:21:21 -0400

    HIDDEN-DRUM.Domain/Catalog/item.cs

---
+++ HIDDEN-DRUM.Domain/Catalog/item.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HIDDEN-DRUM.Domain.Catalog
+{
+    public classs Item
+    {
+        publin int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public strin Brand { get; set; }
+        public decimal Price { get; set; }
+        
+    }
+}
